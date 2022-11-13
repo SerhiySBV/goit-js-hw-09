a51954aae878928162e2a1b3fe77ac97d7ed8f7e
SerhiySBV <serhiybelyavskii@gmail.com>
2022-11-13 20:14:22 +0200

    chenge_color_by_click
    task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const butonStartEl = document.querySelector(`button[data-start]`);
+const butonStopEl = document.querySelector(`button[data-stop]`);
 
+let timer = null;
+
+butonStartEl.addEventListener('click', onStartClick);
+butonStopEl.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  timer = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  butonStartEl.disabled = true;
+}
+function onStopClick() {
+  clearInterval(timer);
+  butonStartEl.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
