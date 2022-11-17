7b8af2faa0e8a0b79af8418fbf691d2becd3a691
SerhiySBV <serhiybelyavskii@gmail.com>
2022-11-17 12:44:49 +0200

    Task_2_Counter

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,27 +1,60 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-import Notiflix from 'notiflix';
+import Notiflix, { Notify } from 'notiflix';
 
-const timerEl = document.querySelector('.timer');
-const dataEL = document.querySelector('[data-days]');
+const daysEL = document.querySelector('[data-days]');
 const hoursEL = document.querySelector('[data-hours]');
 const minutesEL = document.querySelector('[data-minutes]');
 const secondsEL = document.querySelector('[data-seconds]');
 const startButtonEL = document.querySelector('[data-start]');
-const date = new Date();
-
+const dateTimePicker = document.querySelector('#datetime-picker');
+console.log(timerEl);
+startButtonEL.disabled = true;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    console.log(Date.parse(selectedDates[0]));
+    if (Date.parse(selectedDates[0]) <= Date.parse(new Date())) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      startButtonEL.disabled = true;
+      return;
+    }
+
+    startButtonEL.disabled = false;
+
+    function onButtonClick() {
+      dateTimePicker.disabled = true;
+      let timerId = null;
+      timerId = setInterval(() => {
+        let timerCount = convertMs(
+          Date.parse(selectedDates[0]) - Date.parse(new Date())
+        );
+
+        daysEL.textContent = addLeadingZero(timerCount.days);
+        hoursEL.textContent = addLeadingZero(timerCount.hours);
+        minutesEL.textContent = addLeadingZero(timerCount.minutes);
+        secondsEL.textContent = addLeadingZero(timerCount.seconds);
+        console.log(addLeadingZero(secondsEL.textContent));
+
+        if (Date.parse(selectedDates[0]) - Date.parse(new Date()) === 0) {
+          clearInterval(timerId);
+          Notify.success('Time is up!');
+        }
+      }, 1000);
+    }
+    startButtonEL.addEventListener('click', onButtonClick);
     console.log(selectedDates[0]);
   },
 };
 
 const inputEL = flatpickr('#datetime-picker', options);
 
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
 // timerEl.style.dislay = flex;
 // display: grid;
 // grid-template-columns: 50px 50px 60px 60px;
